9524d9ae84b04e5bcdff6ec742e056664bc8085e
Vlad Chuprin <vlastachu@gmail.com>
2016-07-30 22:25:05 +0300

    widget shows feed with useless menu

--- app/src/main/java/me/vlastachu/feedwidget/FeedWidget.java
+++ app/src/main/java/me/vlastachu/feedwidget/FeedWidget.java
@@ -1,20 +1,40 @@
 package me.vlastachu.feedwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.DrawableMarginSpan;
+import android.text.style.ImageSpan;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
-import com.yotadevices.sdk.BackscreenLauncherConstants;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.XMLReader;
 
+import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class FeedWidget extends AppWidgetProvider {
 
@@ -23,22 +43,121 @@ public class FeedWidget extends AppWidgetProvider {
     }
 
     /** Tag for LogCat. */
-    private static final String LOG_TAG = FeedWidget.class.getSimpleName();
+    private static final String TAG             = FeedWidget.class.getSimpleName();
+    private static final String DOWN_CLICKED    = "DOWN_CLICKED";
+    private static final String UP_CLICKED      = "UP_CLICKED";
+    private static final String ITEM_CLICKED    = "ITEM_CLICKED";
+    private static final String PREVIEW_CLICKED = "PREVIEW_CLICKED";
+    private static final String SAVE_CLICKED    = "SAVE_CLICKED";
+    private static final String ROTATE_CLICKED  = "ROTATE_CLICKED";
+    private static final String CLOSE_CLICKED   = "CLOSE_CLICKED";
     private static final Map<Integer, FeedWidgetData> data = new HashMap<>();
 
-    static void updateWidget(Context context, AppWidgetManager widgetManager, int id) {
-        Log.d(LOG_TAG, "updateWidgets: Widget id = " + id);
+    private static final int[] titles = new int[]{R.id.title1, R.id.title2, R.id.title3, R.id.title4};
+    private static final int[] contents = new int[]{R.id.content1, R.id.content2, R.id.content3, R.id.content4};
+    private static final int[] content_menues = new int[]{R.id.content_menu1, R.id.content_menu1,
+            R.id.content_menu1, R.id.content_menu1};
+    private static final int[] preview_buttons = new int[]{R.id.preview_button1, R.id.preview_button1,
+            R.id.preview_button1, R.id.preview_button1};
+    private static final int[] save_buttons = new int[]{R.id.save_button1, R.id.save_button1,
+            R.id.save_button1, R.id.save_button1};
+    private static final int[] rotate_buttons = new int[]{R.id.rotate_button1, R.id.rotate_button1,
+            R.id.rotate_button1, R.id.rotate_button1};
+    private static final int[] close_buttons = new int[]{R.id.close_button1, R.id.close_button1,
+            R.id.close_button1, R.id.close_button1};
+    private static int offset = 0;
+    private static List<FeedItem> items = new ArrayList<>();
+
+    private static class FeedItem {
+        public final String title;
+        public final String content;
+
+        public FeedItem(String title, String content) {
+            this.title = title;
+            this.content = content;
+        }
+    }
+
+    static void updateWidget(final Context context, final AppWidgetManager widgetManager, final int id) {
+        Log.d(TAG, "updateWidgets: Widget id = " + id);
 
         // Get widget options
         //widgetOptions = widgetManager.getAppWidgetOptions(id);
 
-        CharSequence widgetText = SettingsActivity.loadTitlePref(context, id);
-        // Construct the RemoteViews object
-        RemoteViews views = makeView(context);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        final String resourceUrl = SettingsActivity.loadTitlePref(context, id);
 
-        // Instruct the widget manager to update the widget
-        widgetManager.updateAppWidget(id, views);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                run("http");
+            }
+            public void run(String scheme) {
+                try {
+                    URI uri = new URI(resourceUrl);
+                    URL url;
+                    if(uri.getScheme() == null || uri.getScheme().isEmpty()) {
+                        url = new URL(scheme + "://" + resourceUrl);
+                        Log.d(TAG, "run: scheme changed to " + scheme);
+                    }
+                    else {
+                        url = new URL(resourceUrl);
+                    }
+                    InputStream is = url.openStream();
+                    Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                                                         .parse(is).getDocumentElement();
+                    Element channel = (Element)root.getElementsByTagName("channel").item(0);
+                    NodeList items = channel.getElementsByTagName("item");
+                    FeedWidget.items.clear();
+                    for (int i = 0; i < items.getLength(); i++) {
+                        Element item = (Element)items.item(i);
+                        CharacterData title = getTagsContent(item, "title");
+                        CharacterData desc = getTagsContent(item, "description");
+                        FeedWidget.items.add(new FeedItem(title.getData(), desc.getData()));
+                    }
+
+                } catch (Exception e) {
+                    Log.e(TAG, "run exception: " + e + "; message: " + e.getMessage());
+                    if(scheme.equals("http"))
+                        run("https");
+                    return;
+                }
+                //views.setTextViewText(R.id.appwidget_text, widgetText);
+                updateView(context, -1);
+            }
+        }).start();
+    }
+
+    private static CharacterData getTagsContent(Element element, String tagName) {
+        return (CharacterData) element.getElementsByTagName(tagName).item(0).getFirstChild();
+    }
+
+    private static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, FeedWidget.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+        if (UP_CLICKED.equals(action) || DOWN_CLICKED.equals(action)) {
+            if (UP_CLICKED.equals(intent.getAction())) {
+                if (offset > 0)
+                    offset -= titles.length;
+            } else {
+                offset += titles.length;
+            }
+            updateView(context, -1);
+        }
+        if(action.startsWith(ITEM_CLICKED)) {
+            int number = Integer.parseInt(action.substring(ITEM_CLICKED.length()));
+            updateView(context, number);
+        }
+        if(action.startsWith(PREVIEW_CLICKED) || action.startsWith(SAVE_CLICKED) ||
+                action.startsWith(ROTATE_CLICKED) || action.startsWith(CLOSE_CLICKED)) {
+            updateView(context, -1);
+        }
     }
 
     /**
@@ -70,15 +189,58 @@ public class FeedWidget extends AppWidgetProvider {
     /**
      * Make view for widget.
      * */
-    private static RemoteViews makeView(Context context) {
-        // Create RemoteViews
-        return new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+    private static void updateView(Context context, int showMenu) {
+        final int[] ids = getWidgetIds(context);
+        AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        views.setOnClickPendingIntent(R.id.button_up, getPendingSelfIntent(context, UP_CLICKED));
+        views.setOnClickPendingIntent(R.id.button_down, getPendingSelfIntent(context, DOWN_CLICKED));
+
+        for (int i = 0; i < contents.length; i++) {
+            views.setOnClickPendingIntent(contents[i], getPendingSelfIntent(context,
+                    ITEM_CLICKED + i));
+        }
+
+        for (int i = 0; i < titles.length; i++) {
+            if (i + offset < items.size()) {
+                FeedItem item = items.get(i + offset);
+                views.setTextViewText(titles[i], item.title);
+                views.setTextViewText(contents[i], Html.fromHtml(removeImagesAndNewlines(item.content)));
+                views.setViewVisibility(content_menues[i], View.GONE);
+            }
+        }
+
+        if (showMenu != -1) {
+            Log.w(TAG, "updateView: shkshk");
+            int n = showMenu;
+            views.setViewVisibility(content_menues[n], View.VISIBLE);
+            views.setOnClickPendingIntent(preview_buttons[n], getPendingSelfIntent(context,
+                    PREVIEW_CLICKED + n));
+            views.setOnClickPendingIntent(save_buttons[n], getPendingSelfIntent(context,
+                    SAVE_CLICKED + n));
+            views.setOnClickPendingIntent(rotate_buttons[n], getPendingSelfIntent(context,
+                    ROTATE_CLICKED + n));
+            views.setOnClickPendingIntent(close_buttons[n], getPendingSelfIntent(context,
+                    CLOSE_CLICKED + n));
+        }
+
+        // Instruct the widget manager to update the widget
+        for (int id: ids) {
+            Log.d(TAG, "updateWidgets: Widget id = " + id);
+            widgetManager.updateAppWidget(id, views);
+        }
+    }
+
+    private static String removeImagesAndNewlines(String text) {
+        String filtered = text.replaceAll("(<br\\s?/>|<img\\s+src=\"[^\"]*\"[^(/>)]*/>)","");
+        return filtered;
     }
 
     /**
      * Returns all ids of this widget instances.
      * */
-    private int[] getWidgetIds(Context context) {
+    private static int[] getWidgetIds(Context context) {
         return AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context,
                 FeedWidget.class));
     }
